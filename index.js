11331ff8c5f10e398086789c8ea3e39dbcc12bf8
Esteban Cazorla <ecazorla@gmail.com>
2019-11-09 16:50:56 +0000

    refactored module object

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-const unionSet = (...args) => {
-    this.returnSet = new Set();
-    this.errorList = [];
-
-    // Validate that all parameters are either sets or supersets
-    args.forEach((arg, key) => {
-        if (!(arg instanceof Set)) {
-            this.errorList.push([arg, key]);
+const superSet = {
+    union: (...args) => {
+        this.returnSet = new Set();
+        this.errorList = [];
+    
+        // Validate that all parameters are either sets or supersets
+        args.forEach((arg, key) => {
+            if (!(arg instanceof Set)) {
+                this.errorList.push([arg, key]);
+            }
+        });
+    
+        if (this.errorList.length > 0) {
+            throw new Error(`${this.errorList.map(error => `arg[${error[1]}] ${error[0]} is not a valid type set or superset`).join(', ')}`);
         }
-    });
-
-    if (this.errorList.length > 0) {
-        throw new Error(`${this.errorList.map(error => `arg[${error[1]}] ${error[0]} is not a valid type set or superset`).join(', ')}`);
+    
+        args.forEach(arg => {
+            arg.forEach(value => {
+                this.returnSet.add(value);
+            })
+        });
+    
+        return new Set(this.returnSet);
+    },
+    
+    toArray: (arg) => {
+        if (!(arg instanceof Set)) {
+            throw new Error('argument is not a valid type set');
+        }
+    
+        this.setValues = arg.values();
+        this.outputArray = [];
+    
+        for (let value of this.setValues) {
+            this.outputArray.push(value);
+        }
+    
+        return this.outputArray;
     }
-
-    args.forEach(arg => {
-        arg.forEach(value => {
-            this.returnSet.add(value);
-        })
-    });
-
-    return new Set(this.returnSet);
 }
 
-const toArray = (arg) => {
-    if (!(arg instanceof Set)) {
-        throw new Error('argument is not a valid type set');
-    }
-
-    this.setValues = arg.values();
-    this.outputArray = [];
-
-    for (let value of this.setValues) {
-        this.outputArray.push(value);
-    }
-
-    return this.outputArray;
-}
-
-exports.union = unionSet;
-exports.toArray = toArray;
+module.exports = superSet;
